94bc4e566f0352e4dd6b565e6069e6d4907f0b55
Gustavo Martins <gustavomartins.developer@gmail.com>
2025-11-04 23:22:44 -0300

    ajuste api

--- src/api/contact.ts
+++ src/api/contact.ts
@@ -8,8 +8,16 @@ export const sendContact = async (data: {
   isArchitect: boolean;
 }) => {
   try {
-    // Faz o POST para a nossa própria rota na Vercel que irá adicionar a API key
-    const response = await axios.post("/api/send-contact", data);
+    const response = await axios.post(
+      "https://facchini-api.vercel.app/api/send-contact",
+      data,
+      {
+        headers: {
+          "Content-Type": "application/json",
+          "x-api-key": process.env.CONTACT_API_KEY,
+        },
+      }
+    );
     return { ok: true, data: response.data };
   } catch (error) {
     if (axios.isAxiosError(error)) {
